6343df46d5ab1c2bdd561454090edf6af9412cb0
KienVS <kienpc872009@gmail.com>
2026-01-27 08:20:24 +0700

    initV2

--- PKV/urls.py
+++ PKV/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home_view
 
 urlpatterns = [
+    path("", home_view, name="root"), 
     path('admin/', admin.site.urls),
+    path("home/", home_view, name="home"),
 ]
